a825c5bf4628025040c9a330816bdd473f9053a1
Victor Martinez Jr <victormtzcodes@gmail.com>
2022-05-11 15:44:17 -0400

    added javaScript to complete a todo

--- app.js
+++ app.js
@@ -11,53 +11,66 @@ const showTodos = () => {
   if (!todos) return;
 
   let todoHTML = "";
+
   todos.forEach((todo, i) => {
-    todoHTML = `<div class="single-todo">
+    let isComplete = todo.status === "completed" ? "checked" : "";
+
+    todoHTML += `<div class="single-todo">
             <label for="${i}">
-              <input type="radio" id=${i} class="radio-input"/>
-              <p class="label-text">${todo.name}</p>
+              <input type="checkbox" id=${i} class="radio-input" onclick="completeTodo(this)" ${isComplete}/>
+              <p class="label-text ${isComplete}">${todo.name}</p>
             </label>
             <i class="fa-solid fa-trash-can trash-icon"></i>
             </div>`;
-    // Add todos from local storage to ui
-    todosContainer.insertAdjacentHTML("afterbegin", todoHTML);
-  });
-};
-showTodos();
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  let userTodo = addInput.value.trim();
-
-  if (!userTodo) return;
-
-  // Create empty array if local storage is empty
-  if (!todos) {
-    todos = [];
-  }
-
-  // Clear input
-  addInput.value = "";
-
-  // Todolist data being pushed to todos array
-  let todoInfo = { name: userTodo, status: "pending" };
-  todos.push(todoInfo);
-
-  // Save todos to local storage
-  localStorage.setItem("todo-list", JSON.stringify(todos));
-
-  // Show new todos
-  let todoHTML = "";
-  todos.forEach((todo, i) => {
-    todoHTML = `<div class="single-todo">
-            <label for="${i}">
-              <input type="radio" id=${i} class="radio-input"/>
-              <p class="label-text">${todo.name}</p>
-            </label>
-            <i class="fa-solid fa-trash-can trash-icon"></i>
-          </div>`;
   });
   // Add todos from local storage to ui
-  todosContainer.insertAdjacentHTML("afterbegin", todoHTML);
-});
+  todosContainer.innerHTML = todoHTML;
+};
+
+showTodos();
+
+// Handle complete todo
+const completeTodo = (todo) => {
+  if (!todo) return;
+  let todoName = todo.parentElement.lastElementChild;
+  if (todo.checked) {
+    todoName.classList.add("checked");
+    todos[todo.id].status = "completed";
+  } else {
+    todoName.classList.remove("checked");
+    todos[todo.id].status = "pending";
+  }
+
+  localStorage.setItem("todo-list", JSON.stringify(todos));
+};
+completeTodo();
+
+// Handle form submit
+const handleFormSubmit = () => {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let userTodo = addInput.value.trim();
+
+    if (!userTodo) return;
+
+    // Create empty array if local storage is empty
+    if (!todos) {
+      todos = [];
+    }
+
+    // Clear input
+    addInput.value = "";
+
+    // Todolist data being pushed to todos array
+    let todoInfo = { name: userTodo, status: "pending" };
+    todos.push(todoInfo);
+
+    // Save todos to local storage
+    localStorage.setItem("todo-list", JSON.stringify(todos));
+
+    // Show new todos
+    showTodos();
+  });
+};
+handleFormSubmit();
